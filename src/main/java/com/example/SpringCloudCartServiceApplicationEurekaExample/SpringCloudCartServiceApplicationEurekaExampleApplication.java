5013456dfb6c65355a11ad0767193c4188bfecde
PANKAJ SHARMA <gaur.ps21@outlook.com>
2023-02-12 10:07:19 +0000

    final

---
+++ src/main/java/com/example/SpringCloudCartServiceApplicationEurekaExample/SpringCloudCartServiceApplicationEurekaExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringCloudCartServiceApplicationEurekaExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringCloudCartServiceApplicationEurekaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudCartServiceApplicationEurekaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringCloudCartServiceApplicationEurekaExample/SpringCloudCartServiceApplicationEurekaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCloudCartServiceApplicationEurekaExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCartServiceApplicationEurekaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
